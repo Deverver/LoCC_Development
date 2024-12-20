1d056648b32da098a8d1fef21b0bf2fdc1d4ef5b
Deverver <jackRMrasmussen@gmail.com>
2024-12-20 11:41:51 +0100

    Initial commit

---
+++ src/main/java/com/example/locc_development/Main.java
@@ -0,0 +1,42 @@
+package com.example.locc_development;
+
+import com.example.locc_development.Controller.MenuController;
+import com.example.locc_development.DBController.DatabaseConnection;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    MenuController menuController = new MenuController();
+
+    /*
+    try {
+        DatabaseConnection.createFiles();
+    } catch (IOException e2) {
+        throw new RuntimeException(e2.getMessage());
+    } catch (Exception e3) {
+        e3.printStackTrace();
+    }
+
+    boolean run = true;
+        menuController.menu(run);
+  */
+
+
+    // JavaFX scene builder start
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
